67d6e6fbec56c2b292b3ca411752740d09b58247
posen <abc.ex@it>
2019-02-15 01:23:13 +0100

    add feature

--- Metaclass.py
+++ Metaclass.py
@@ -1,26 +1,31 @@
 
-
-class Student:
+class Person:
     
     def __init__(self, name, lastname, birthday):
-        self._name = name
-        self._lname = lastname
-        self._birthday = birthday
+        self.name = name
+        self.lastname = lastname
+        self.birthday = birthday
     
     def __setattr__(self, obj, value):
-        self.__dict__["_"+obj] = value
+        self.__dict__[obj] = value
+        
 
     def __getattr__(self, obj):
-        return getattr(self,"_"+obj)
+        return getattr(self, obj)
 
-    
+    def __repr__(self):
+        return str("Nome: "+self.name+ "\nCognome: "+self.lastname+"\nData di nascita: "+self.birthday)
+
+class Student(Person):
+
+    def __init__(self):
+        self._dict={}
 
 
 if __name__ == "__main__":
-    s= Student("luca","Colacioppo", "13/07/1994")
-    s.name="Daniele"
-    print(s.name)
-
-
-
+    p = Person("luca","Colacioppo", "13/07/1994")
+    print(p)
+    i1 = str(input("inserisci un nome diverso: "))
+    p.name = i1
+    print(p)
 
