755ecc14b49fe3db908be5d8eecbaf7ea90929e7
posen <abc.ex@it>
2019-02-15 23:49:09 +0100

    i did exercise one point one

--- Metaclass.py
+++ Metaclass.py
@@ -1,3 +1,4 @@
+import functools
 
 class Person:
     
@@ -9,23 +10,41 @@ class Person:
     def __setattr__(self, obj, value):
         self.__dict__[obj] = value
         
-
     def __getattr__(self, obj):
         return getattr(self, obj)
 
     def __repr__(self):
         return str("Nome: "+self.name+ "\nCognome: "+self.lastname+"\nData di nascita: "+self.birthday)
 
+
+
 class Student(Person):
 
-    def __init__(self):
-        self._dict={}
+    def __init__(self, name, lastname, birthday, esamivoti = {} ):
+        super().__init__(name, lastname, birthday)
+        self.exam={}
+        self.exam.update(esamivoti)
+
+
+    def __repr__(self):
+        return str("Nome: "+self.name+ "\nCognome: "+\
+            self.lastname+"\nData di nascita: "+self.birthday+\
+            "\nExams: \n"+str(list(self.exam.items())))
+
+    @property    
+    def get_avarage(self):
+        tipo = list(self.exam.values())
+        return functools.reduce(lambda x,y: x+y, tipo)/len(tipo)
 
 
 if __name__ == "__main__":
-    p = Person("luca","Colacioppo", "13/07/1994")
+    p = Student("luca","Colacioppo", "13/07/1994",{"matematica":20})
+    p.exam.update({"GeoStoria": 27})
     print(p)
-    i1 = str(input("inserisci un nome diverso: "))
-    p.name = i1
-    print(p)
+    print(p.get_avarage)
 
+
+
+    #i1 = str(input("inserisci un nome diverso: "))
+    #p.name = i1
+    #print(p)   
